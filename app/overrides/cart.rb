73908c9e3c7a65038317b7267e877a0332979556
evan <transcendentalguy@gmail.com>
2017-09-25 14:54:15 -0400

    updated storefront

---
+++ app/overrides/cart.rb
@@ -0,0 +1,4 @@
+# Deface::Override.new(:virtual_path => "spree/orders/edit",
+#                      :replace => "div.flub",
+#                      :text         => "<p>Registration is the future!</p>",
+#                      :name         => "registration_future")
